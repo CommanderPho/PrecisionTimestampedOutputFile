09b798465d7dc32cae492957e67d3e136bc37b27
Pho Hale <PhoHale@gmail.com>
2021-11-29 13:16:00 -0500

    updated install plugin

--- HelperScripts/InstallPlugin.cmd
+++ HelperScripts/InstallPlugin.cmd
@@ -2,20 +2,28 @@
 @REM cp "C:\Users\Pho\repos\OpenEphys\OEPlugins\PrecisionTimestampedOutputFile\Build\RelWithDebInfo\PrecisionTimestampedOutputFile.dll" "C:\ProgramData\Open Ephys\plugins\PrecisionTimestampedOutputFile.dll"
 
 
-set PLUGIN_DIR=C:\Users\Pho\repos\OpenEphys\OEPlugins\PrecisionTimestampedOutputFile\Build\Debug
+@REM set PLUGIN_DIR=C:\Users\Pho\repos\OpenEphys\OEPlugins\PrecisionTimestampedOutputFile\Build\Debug
+set PLUGIN_DIR=C:\Users\Pho\repos\OpenEphys\OEPlugins\PrecisionTimestampedOutputFile\Build\Release
+
+
 @REM Official Release
-set PROGRAM_PLUGINS_PATH=C:\ProgramData\Open Ephys\plugins
+set OFFICIAL_PROGRAM_PLUGINS_PATH=C:\ProgramData\Open Ephys\plugins
 @REM Debug
 set PROGRAM_PLUGINS_PATH=C:\Users\Pho\repos\OpenEphys\plugin-GUI\Build\Debug\plugins
 
 @REM cp "C:\Users\Pho\repos\OpenEphys\OEPlugins\PrecisionTimestampedOutputFile\Build\Debug\PrecisionTimestampedOutputFile.pdb" "C:\ProgramData\Open Ephys\plugins\PrecisionTimestampedOutputFile.dll"
 
 echo Installing to %PROGRAM_PLUGINS_PATH%
-copy "C:\Users\Pho\repos\OpenEphys\OEPlugins\PrecisionTimestampedOutputFile\Build\Debug\PrecisionTimestampedOutputFile.dll" "%PROGRAM_PLUGINS_PATH%\PrecisionTimestampedOutputFile.dll"
-copy "C:\Users\Pho\repos\OpenEphys\OEPlugins\PrecisionTimestampedOutputFile\Build\Debug\PrecisionTimestampedOutputFile.pdb" "%PROGRAM_PLUGINS_PATH%\PrecisionTimestampedOutputFile.pdb"
-
+copy "%PLUGIN_DIR%\PrecisionTimestampedOutputFile.dll" "%PROGRAM_PLUGINS_PATH%\PrecisionTimestampedOutputFile.dll"
+copy "%PLUGIN_DIR%\PrecisionTimestampedOutputFile.pdb" "%PROGRAM_PLUGINS_PATH%\PrecisionTimestampedOutputFile.pdb"
 echo Done
 
-pause
+
+echo Installing to %OFFICIAL_PROGRAM_PLUGINS_PATH%
+copy "%PLUGIN_DIR%\PrecisionTimestampedOutputFile.dll" "%OFFICIAL_PROGRAM_PLUGINS_PATH%\PrecisionTimestampedOutputFile.dll"
+copy "%PLUGIN_DIR%\PrecisionTimestampedOutputFile.pdb" "%OFFICIAL_PROGRAM_PLUGINS_PATH%\PrecisionTimestampedOutputFile.pdb"
+echo Done
+
+@REM pause
 
 
