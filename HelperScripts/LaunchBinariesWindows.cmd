52978620c09ab7efdd42328d4e9aeb691a40ed27
Pho Hale <PhoHale@gmail.com>
2024-03-28 10:01:38 -0400

    bump

--- HelperScripts/LaunchBinariesWindows.cmd
+++ HelperScripts/LaunchBinariesWindows.cmd
@@ -1,5 +1,10 @@
-cd "C:\Users\Pho\repos\OpenEphys\plugin-GUI\Build\Debug"
-start "C:\Users\Pho\repos\OpenEphys\plugin-GUI\Build\Debug\open-ephys.exe"
+setlocal
+
+set ReleaseMode=Debug
+cd "C:\Users\Pho\repos\OpenEphys\plugin-GUI\Build\%ReleaseMode%"
+start "C:\Users\Pho\repos\OpenEphys\plugin-GUI\Build\%ReleaseMode%\open-ephys.exe"
 
 
 start "C:\ProgramData\Open Ephys\plugins" .
+
+endlocal
